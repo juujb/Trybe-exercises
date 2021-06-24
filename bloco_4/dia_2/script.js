02530eb3ce7a20fcee29d62f958b01470bd441d8
Jucélio <juceneon@gmail.com>
2021-06-24 18:40:49 -0300

    Exercicio 6

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,14 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorNumero = 0;
+let numerosImpares = 0;
 
 for (let index = 0; index < numbers.length; index++) {
     
-        if (numbers[index] > maiorNumero) {  
-            maiorNumero = numbers[index]
-        }
+    if (numbers[index] % 2 != 0) {  
+        numerosImpares++
     }
+}
 
-
-
-console.log(maiorNumero)
+if (numerosImpares > 0) {
+    console.log(numerosImpares)
+} else {
+    console.log("Nenhum valor ímpar encontrado")
+}
