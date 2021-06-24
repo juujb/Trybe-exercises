d473306f0eaac9f8b2cecba8d009ddb725364a94
Jucélio <juceneon@gmail.com>
2021-06-24 18:56:38 -0300

    Exercicio 9

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,8 +1,13 @@
 let array = [];
 
+let metadeDosNumeros = [];
+
 for (let index = 0; index < 25; index++) {
 
     array.push(index+1);
+
+    metadeDosNumeros.push(array[index]/2);
 }
 
 console.log(array);
+console.log(metadeDosNumeros);
