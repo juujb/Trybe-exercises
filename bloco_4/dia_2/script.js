c0b15c5ad4a695c33e973b046217aae781e0b661
Jucélio <juceneon@gmail.com>
2021-06-24 18:48:07 -0300

    Exercicio 8

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,21 +1,8 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let array = [];
 
-let maiorNumero = 0;
+for (let index = 0; index < 25; index++) {
 
-for (let index = 0; index < numbers.length; index++) {
-    
-    if (numbers[index] > maiorNumero) {  
-        maiorNumero = numbers[index]
-    }
+    array.push(index+1);
 }
 
-let menorNumero = maiorNumero
-
-for (let index = 0; index < numbers.length; index++) {
-    
-    if (numbers[index] < menorNumero) {  
-        menorNumero = numbers[index]
-    }
-}
-
-console.log(menorNumero);
+console.log(array);
