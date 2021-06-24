a23fab1f4a75ace06522afc1452ded4664cdce1f
Jucélio <juceneon@gmail.com>
2021-06-24 18:36:27 -0300

    Exercicio 5

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -1,19 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0;
+let maiorNumero = 0;
 
 for (let index = 0; index < numbers.length; index++) {
-    soma += numbers[index];
-}
+    
+        if (numbers[index] > maiorNumero) {  
+            maiorNumero = numbers[index]
+        }
+    }
 
-let media = soma / numbers.length
 
-console.log(soma);
 
-console.log(media);
-
-if (media > 20){
-    console.log("Valor maior que 20")
-} else {
-    console.log("Valor menor que 20")
-}
+console.log(maiorNumero)
