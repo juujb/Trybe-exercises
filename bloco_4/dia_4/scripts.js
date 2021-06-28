697f65cc04469e38cde0cec03f9bd9d56090d46f
Jucélio <juceneon@gmail.com>
2021-06-28 19:21:13 -0300

    Função que verifica o indice do menor numero de uma array

--- bloco_4/dia_4/scripts.js
+++ bloco_4/dia_4/scripts.js
@@ -1,14 +1,14 @@
-let array = [2, 3, 6, 7, 10, 1];
+let array = [2, 4, 6, 7, 10, 0, -3];
 
-function maiorNumero(array) {
+function menorNumero(array) {
     
     let indiceDoNumero;
     for (let index = 0; index < array.length; index++) {
-        if (array[index] > array[index+1]){
+        if (array[index] < array[index-1]){
             indiceDoNumero = index
         }
     }
     return console.log(indiceDoNumero)
 }
 
-maiorNumero(array)
+menorNumero(array)
