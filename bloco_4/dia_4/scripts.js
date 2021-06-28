8dcf06d9724de7a03f3f19564191bccefc6114ce
Jucélio <juceneon@gmail.com>
2021-06-28 18:57:24 -0300

    Imprimir texto sobre as informações de ambos personagens

--- bloco_4/dia_4/scripts.js
+++ bloco_4/dia_4/scripts.js
@@ -5,6 +5,24 @@ let info = {
     recorrente: 'Sim',
 };
 
-for (const key in info) {
-    console.log(info[key])
+
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+
+if (info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    console.log('Ambos recorrentes')
+} else if  (info.recorrente ==='Sim' || info2.recorrente === 'Sim'){
+    console.log('Apenas um é recorrente')
+}else{
+    console.log('Nenhum recorrente')
 }
+
