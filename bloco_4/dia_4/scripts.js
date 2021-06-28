d5da4f396fc551e619cf10b1a41fa2aa7804a770
Jucélio <juceneon@gmail.com>
2021-06-28 19:10:59 -0300

    Função que verifica se a palavra é um palindromo

--- bloco_4/dia_4/scripts.js
+++ bloco_4/dia_4/scripts.js
@@ -1,28 +1,21 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
-};
 
 
+function verificaPalindrome(word) {
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-};
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
 
-console.log(info.personagem + ' e ' + info2.personagem);
-console.log(info.origem + ' e ' + info2.origem);
-console.log(info.nota + ' e ' + info2.nota);
+    
+    if (word === reverseWord) {
+        palindrome = true
+    } else {
+        palindrome = false
+    }
 
-if (info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
-    console.log('Ambos recorrentes')
-} else if  (info.recorrente ==='Sim' || info2.recorrente === 'Sim'){
-    console.log('Apenas um é recorrente')
-}else{
-    console.log('Nenhum recorrente')
+    return console.log(palindrome)
 }
+
+verificaPalindrome('arara');
 
