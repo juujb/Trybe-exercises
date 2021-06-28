7b6c8579daf6afe7754c428509bbf5bacbf45038
Jucélio <juceneon@gmail.com>
2021-06-28 18:37:58 -0300

    Initial commit

--- bloco_4/dia_3/script.js
+++ bloco_4/dia_3/script.js
@@ -1,8 +1,19 @@
 let n = 5;
-let figura = "";
 
-for (let index = 0; index < n; index++) {
-    figura = figura + "*"
-    console.log(figura);
+for (let i = 0; i < n; i++) {
+    for (let z = 1; z >= (i/2)-1; z--) {
+        console.log("*");
+    }
+
+    for (let x = 0; x <= (i*2)-i; x++) {
+        console.log(i+1);
+    }
+
+    for (let y = 1; y >= (i/2)-1; y--) {
+        console.log("*");
+
+    }
+
+    i+=1;
+
 }
-
