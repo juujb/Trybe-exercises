1b25f84628581ce000dca42a2613a1568e8d2e35
Jucélio <juceneon@gmail.com>
2021-07-22 18:35:33 -0300

    Parte I Exercicio 2

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -1,13 +1,5 @@
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
-}
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-testingScope(true);
+const orderArray = (array) => array.sort((a, b) => a - b);
+
+console.log(`Os números ${orderArray(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
