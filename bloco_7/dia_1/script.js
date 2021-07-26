cd3fe59ca211a3a5174b8d89115b19921fa4bc09
Jucélio <juceneon@gmail.com>
2021-07-26 19:24:03 -0300

    Initial Commit

--- bloco_7/dia_1/script.js
+++
@@ -1,5 +0,0 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-const orderArray = (array) => array.sort((a, b) => a - b);
-
-console.log(`Os números ${orderArray(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
--- bloco_7/dia_2/script.js
+++ bloco_7/dia_2/script.js
@@ -1,66 +1,45 @@
-const order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
-  address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
-  },
-  order: {
-    pizza: {
-      marguerita: {
-        amount: 1,
-        price: 25,
-      },
-      pepperoni: {
-        amount: 1,
-        price: 20,
-      }
-    },
-    drinks: {
-      coke: {
-        type: 'Coca-Cola Zero',
-        price: 10,
-        amount: 1,
-      }
-    },
-    delivery: {
-      deliveryPerson: 'Ana Silveira',
-      price: 5,
-    }
-  },
-  payment: {
-    total: 60,
-  },
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
-const customerInfo = () => {
-  const orderInfo = Object.assign({}, order.order);
-  const deliveryInfo = Object.assign({}, orderInfo.delivery);
-  const deliveryPersonName = deliveryInfo.deliveryPerson;
-  const clientName = order.name;
-  const clientNumber = order.phoneNumber;
-  const clientAdress = Object.assign({}, order.address);
-  const addressArray = Object.values(clientAdress);
-  console.log(`Olá ${deliveryPersonName}, entrega para: ${clientName}, Telefone: ${clientNumber}, ${addressArray[0]}, Nº: ${addressArray[1]}, AP: ${addressArray[2]}`);
-}
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
 
-customerInfo();
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
 
-const orderModifier = () => {
-  const newOrder = Object.assign({}, order);
-  const orderInfo = Object.assign({}, newOrder.order);
-  const pizzas = Object.assign({}, orderInfo.pizza);
-  const drinks = Object.assign({}, orderInfo.drinks);
-  newOrder.name = 'Luiz Silva';
-  let payment = 0;
-  for (const key in pizzas) {
-    const pizza = pizzas[key];
-    payment += pizza.price;
-  }
-  payment += drinks.coke.price;
-  newOrder.payment = payment;
-  console.log(`Olá ${newOrder.name}, o total do seu pedido de ${Object.keys(pizzas)[0]}, ${Object.keys(pizzas)[1]} e ${Object.keys(drinks)} é de R$ ${newOrder.payment},00.`);
-}
+const addTurnToObject = (object, key, value) => object[key] = value;
 
-orderModifier();
+const listKeys = (object) => {for(const key in object){console.log(key)}};
+
+const objectLength = (object) => console.log(Object.keys(object).length);
+
+const listValues = (object) => {for(let index of Object.values(object)){console.log(index)}};
+
+let allLessons = {};
+
+const key1 = Object.assign(allLessons, lesson1);
+
+Object.assign(allLessons, lesson2);
+
+Object.assign(allLessons, lesson3);
+
+addTurnToObject(lesson2, 'turno', 'manhã');
+
+listKeys(lesson1);
+
+objectLength(lesson3);
+
+listValues(lesson2);
+
+console.log(allLessons);
