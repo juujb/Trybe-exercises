65d6e02a7b5196ab0295470c52382cf08c559f3e
Jucélio <juceneon@gmail.com>
2021-07-28 19:32:43 -0300

    exercicio 2

--- bloco_8/dia_1/script.js
+++ bloco_8/dia_1/script.js
@@ -4,17 +4,41 @@ const createEmail = (name) => {
   return id;
 };
 
-console.log(createEmail('Jucelio Brandao'));
-
-const newEmployees = () => {
+const newEmployees = (callBack) => {
   const employees = {
     id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
     id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
     id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
-  employees.id1 = createEmail('Pedro Guerra');
-  employees.id2 = createEmail('Luiza Drummond');
-  employees.id3 = createEmail('Carla Paiva');
+  employees.id1 = callBack('Pedro Guerra');
+  employees.id2 = callBack('Luiza Drummond');
+  employees.id3 = callBack('Carla Paiva');
 
   return employees;
 };
+
+console.log(newEmployees(createEmail));
+
+const randomNum = () => {
+  const min = Math.ceil(1);
+  const max = Math.floor(5);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const isEqual = (number) => randomNum() === number? true:false ;
+
+
+const fortuneResult = (luck, callBack) => {
+  if (callBack(luck) === true) {
+    console.log(callBack(luck));
+    return 'Parabéns você ganhou';
+  } else {
+    
+    return 'Tente novamente';
+  }
+}
+
+console.log(fortuneResult(3,isEqual));
+
