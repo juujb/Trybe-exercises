2dff559689fdd9de501a1c2c5290636059a0bf86
Jucélio <juceneon@gmail.com>
2021-08-17 18:44:02 -0300

    Impl: myRemove tests

--- bloco_10/dia_1/test/parte1.spec.js
+++ bloco_10/dia_1/test/parte1.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const { sum } = require('../src/parte1');
+const { myRemove } = require('../src/parte1');
 
 describe('A função sum', () => {
   test('soma corretamente?', () => {
@@ -12,4 +13,17 @@ describe('A função sum', () => {
   test('retorna "parameters must be numbers" como erro?', () => {
     expect(() => sum(4, '5')).toThrow('parameters must be numbers');
   })
+});
+
+describe('A função myRemove', () => {
+  test('remove o elemento esperado?', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('não altera o array passado(sem um elemento a ser removido)?', () => {
+    expect(myRemove([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+  test('caso o elemento a ser removido não exista, retorna o array esperado?', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
