37519f13c30ccf09e6528219ef73d2ae4895ab1c
Jucélio Brandão <juceneon@gmail.com>
2021-08-18 20:49:13 -0300

    Impl: Exercise 2

--- bloco_10/dia_2/test/script.test.js
+++ bloco_10/dia_2/test/script.test.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const { uppercase } = require('../src/script');
+const { getUserName } = require('../src/script');
+const { findUserById } = require('../src/script');
 
 describe('A função "uppercase":', () => {
   test('Transforma as letras em letras maiúsculas?', (done) => {
@@ -11,5 +13,17 @@ describe('A função "uppercase":', () => {
         done(error);
       }
     });
+  });
+});
+
+describe('A função "getUserName":', () => {
+  test('Encontra o usuario correto', (done) => {
+    getUserName(1).then((user) => {
+      expect(user).toBe('Marki');
+      done();
+    });
+  });
+  test('NÃO encontra o usuario correto', () => {
+    
   });
 });
